f61f65c2452a8ece67e8f3be1da663771170057a
jorgejaimem8 <845067@unizar.es>
2024-02-16 13:22:59 +0100

    cambios main para que este completo

--- practica2_845067_840710/c++/main.cc
+++ practica2_845067_840710/c++/main.cc
@@ -2,12 +2,30 @@
 #include <iostream>
 #include "sumaProg.h"
 #include "cuentaAtras.h"
+#include "factorial.h"
 
 int main(){
+    std::stack<int>* pila = new std::stack<int>;
     std::cout << "Ejecución de la suma de dos números" << std::endl;
     std::cout << "-----------------------------------" << std::endl;
-    std::stack<int>* pila = new std::stack<int>;
-    Cuenta sum(pila);
+    Suma sum(pila);
     sum.listarse(std::cout);
+    std::cout << std::endl;
     sum.ejecutar(std::cout);
+    
+    std::cout << std::endl << std::endl << "Ejecución de la cuenta atrás" << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+    Cuenta cuenta(pila);
+    cuenta.listarse(std::cout);
+    std::cout << std::endl;
+    cuenta.ejecutar(std::cout);
+
+
+
+    std::cout << std::endl << std::endl << "Ejecución del factorial" << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+    Factorial fact(pila);
+    fact.listarse(std::cout);
+    std::cout << std::endl;
+    fact.ejecutar(std::cout);
 }
